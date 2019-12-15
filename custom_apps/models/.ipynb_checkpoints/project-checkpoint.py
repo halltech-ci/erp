b55f82f9f7953d1ef64eb6fb5ae30b258c6b84be
halltech-ci <54378200+halltech-ci@users.noreply.github.com>
2019-12-15 17:52:20 +0000

    Added project link in purchase order

---
+++ custom_apps/models/.ipynb_checkpoints/project-checkpoint.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+# class custom_apps(models.Model):
+#     _name = 'custom_apps.custom_apps'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
+
+class PurchaseOrder(models.Model):
+    _inherit = "project.project"
+    
+    purchase_order_ids = fields.One2many("purchase.order", 
+                                         "project_id", 
+                                         string="Purchase Order"
+    )
--- custom_apps/models/__init__.py
+++ custom_apps/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+#from . import models
 from . import purchase_order
+from . import project
